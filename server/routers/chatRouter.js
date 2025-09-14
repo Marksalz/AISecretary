79cae64b497834c76f883f2210c2c95dbf506379
Mark <menachemsalzberg13@gmail.com>
2025-09-14 16:17:00 +0300

    feat: add authentication middleware to chat route

--- server/routers/chatRouter.js
+++ server/routers/chatRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { chatController } from "../controllers/chatController.js";
+import { requireAuth } from "../middleware/middleware.js";
 
 const chatRouter = express.Router();
 
 // Map POST /chat to the controller
-chatRouter.post("/", chatController);
+// Add auth middleware to POST /chat
+chatRouter.post("/", requireAuth, chatController);
 
 export default chatRouter;
