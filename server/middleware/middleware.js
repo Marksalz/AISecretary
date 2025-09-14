8ccb6706d794a9c8b2e77e9f63a9736d1679609c
erez cohen <erezjbh@gmail.com>
2025-09-14 16:45:05 +0300

    add middleware and chat

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,13 +1,15 @@
 import jwt from "jsonwebtoken";
 
 export function requireAuth(req, res, next) {
-  const token = req.cookies?.token;
+  const token = req.cookies?.token;  
   if (!token) {
     return res.status(401).json({ error: "No token provided" });
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+    console.log('in middleware');
+    
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid token" });
