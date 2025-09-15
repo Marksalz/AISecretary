099c6a9bc562e0df5900072d0986fc16c8a66559
Mark <menachemsalzberg13@gmail.com>
2025-09-15 16:28:25 +0300

    Refactor Header component for improved readability; update main routing to use Welcome page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,15 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InputChat from "./pages/InputChat";
 import Layout from "../Layout-app/Layout.js";
-import App from "./App.js";
+import Welcome from "./pages/Welcome.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Welcome />} />
           <Route path="/chat" element={<InputChat />} />
-          {/* <Route path="/Home" element={<InputChat />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
